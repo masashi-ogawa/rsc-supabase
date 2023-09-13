35a7208e88bed73aaa17ddc9f907628d8e2735a4
Masashi Ogawa <masashi.ogawa@lubricate.dev>
2023-09-13 12:57:59 +0900

    add Suspence

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import NotesList from '@/app/components/notex-list'
 import TimerCounter from '@/app/components/timer-counter'
+import { Suspense } from 'react'
+import Spinner from '@/app/components/spinner'
 
 export default function Page() {
   return (
     <main>
       <div className="m-10 text-center">
         <p>Hello World🚀</p>
-        <NotesList />
+        <Suspense fallback={<Spinner color="border-green-500" /> }>
+          <NotesList />
+        </Suspense>
         <TimerCounter />
       </div>
     </main>
